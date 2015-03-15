142047e1b29f517bf23324382a7739dbb2206dc8
yahsharahla <artnellebanks@gmail.com>
2015-03-15 10:06:38 +0000

    tutorial 3

--- js/game.js
+++ js/game.js
@@ -1,51 +1,59 @@
-
-/* Game namespace */
+/* game namespace */
 var game = {
-
-    // an object where to store game information
-    data : {
-        // score
-        score : 0
-    },
-
-
-    // Run on page load.
-    "onload" : function () {
+ 
+  /**
+  * an object where to store game global data
+  */
+ 
+  data : {
+    score : 0
+  },
+ 
+  // Run on page load.
+  "onload" : function () {
+ 
     // Initialize the video.
-    if (!me.video.init("screen",  me.video.CANVAS, 960, 640, true, 'auto')) {
-        alert("Your browser does not support HTML5 canvas.");
-        return;
+    if (!me.video.init("screen", me.video.CANVAS, 640, 480, true, 'auto')) {
+      alert("Your browser does not support HTML5 canvas.");
+      return;
     }
-
+ 
     // add "#debug" to the URL to enable the debug Panel
     if (document.location.hash === "#debug") {
-        window.onReady(function () {
-            me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
-        });
+      window.onReady(function () {
+        me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
+      });
     }
-
+ 
     // Initialize the audio.
     me.audio.init("mp3,ogg");
-
+ 
     // Set a callback to run when loading is complete.
     me.loader.onload = this.loaded.bind(this);
-
+ 
     // Load the resources.
     me.loader.preload(game.resources);
-
+ 
     // Initialize melonJS and display a loading screen.
     me.state.change(me.state.LOADING);
-},
+  },
+ 
+  // Run on game resources loaded.
+  "loaded" : function () {
+    me.state.set(me.state.MENU, new game.TitleScreen());
+    // set the "Play/Ingame" Screen Object
+    me.state.set(me.state.PLAY, new game.PlayScreen());
 
-    // Run on game resources loaded.
-    "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
-        me.state.set(me.state.PLAY, new game.PlayScreen());
+    // register our player entity in the object pool
+    me.pool.register("mainPlayer", game.PlayerEntity);
 
-        // add our player entity in the entity pool
-        me.pool.register("mainPlayer", game.PlayerEntity);
+    // enable the keyboard
+    me.input.bindKey(me.input.KEY.LEFT,  "left");
+    me.input.bindKey(me.input.KEY.RIGHT, "right");
+    me.input.bindKey(me.input.KEY.X,     "jump", true);
 
-        // Start the game.
-        me.state.change(me.state.PLAY);
-    }
+    // start the game
+    me.state.change(me.state.PLAY);
+  }
 };
+
